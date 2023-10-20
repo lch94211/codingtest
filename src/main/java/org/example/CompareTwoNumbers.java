446410e49ed31faed55388b4ef37b5ef8ed2249a
lch94 <lch94211@naver.com>
2023-10-20 22:40:44 +0900

    codingtest2

---
+++ src/main/java/org/example/CompareTwoNumbers.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class CompareTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a < b){
+            System.out.println("<");
+        }else if(a > b) {
+            System.out.println(">");
+        }else {
+            System.out.println("==");
+        }
+
+
+
+    }
+}
---
+++ src/main/java/org/example/LeapYear.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+
+    }
+}
