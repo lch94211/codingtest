da39db82d4185212b67d377fe99b25033aebd449
lch94 <lch94211@naver.com>
2023-10-26 21:34:35 +0900

    codingtest4

---
+++ src/main/java/org/example/Star.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Star {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        StringBuffer b = new StringBuffer(); //StringBuffer 는 문자열을 효율적으로 처리할수있다
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                b.append("*");
+            }
+            b.append("\n");
+        }
+
+        System.out.println(b);
+
+    }
+}
+
